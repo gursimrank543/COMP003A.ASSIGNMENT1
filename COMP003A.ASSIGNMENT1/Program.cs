311481ec540f2d59ed401269a3b9998cb7c3019b
Gursimran kaur <Gursimran kaur@SIMRANBHULLAR>
2024-01-21 23:51:46 -0800

    Add project files.

---
+++ COMP003A.ASSIGNMENT1/Program.cs
@@ -0,0 +1,98 @@
+﻿//
+/*
+ * 
+ * Author: Gursimran Kaur
+ * Course: COMP-003A-L01
+ * Purpose: COMP003A.ASSIGNMENT1
+ */
+ namespace COMP003A.ASSIGNMENT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Gursimran Kaur");
+            Console.WriteLine("My Grandmother gave me this name");
+            Console.WriteLine("I love my name as it means remembering the lord");
+            Console.WriteLine("I born in India");
+            Console.WriteLine("I am 20 years old");
+            Console.WriteLine("I belong to a middle-class family");
+            Console.WriteLine("I am the elder daughter of my parents");
+            Console.WriteLine("I have one brother named Gurjinder");
+            Console.WriteLine("My father name is Satwinder");
+            Console.WriteLine("My Mother name is Kulwinder");
+            Console.WriteLine("I love all my family members");
+            Console.WriteLine("I am an Extrovert person");
+            Console.WriteLine("I value friendship and people in my life");
+            Console.WriteLine("My friends describe me as a humorous person");
+            Console.WriteLine("My favorite color is black");
+            Console.WriteLine("I love my personality");
+            Console.WriteLine("I do play games in my free time");
+            Console.WriteLine("My hobbies include painting and reading books");
+            Console.WriteLine("I like to read different novels");
+            Console.WriteLine("I prefer reading history");
+            Console.WriteLine("I also love playing indoor games with my brother");
+            Console.WriteLine("I like taking risk in life");
+            Console.WriteLine("My nature is entirely adventurous");
+            Console.WriteLine("I take risks and do not fear the circumstances");
+            Console.WriteLine("My interests and reality are not the same");
+            Console.WriteLine("I did my high school in India");
+            Console.WriteLine("I participated in many competitions");
+            Console.WriteLine("Participating in any competition gave me confidence");
+            Console.WriteLine("I've learned at a very young age to never judge people");
+            Console.WriteLine(" I enjoy working in a group");
+            Console.WriteLine("After my high school, I did Ielts");
+            Console.WriteLine("I got 7 Bands in Ielts test");
+            Console.WriteLine("I gave interview and then I got Visa of United States");
+            Console.WriteLine("Just 2 years ago I came to United State");
+            Console.WriteLine("When I came here Everything was new for me");
+            Console.WriteLine("My friends were not with me");
+            Console.WriteLine("I was feeling lonely");
+            Console.WriteLine("My relatives guided me and supported me");
+            Console.WriteLine("I experienced a lot of things by moving to this country");
+            Console.WriteLine("It was difficult for me to survive here");
+            Console.WriteLine("I was not knowing about rules and regulations");
+            Console.WriteLine("Slowly I met new people and learned many new things");
+            Console.WriteLine("I was not knowing Spanish");
+            Console.WriteLine("I use to take help from translator");
+            Console.WriteLine("With the help of online source I learned spanish as well");
+            Console.WriteLine("Meeting different people has opened my mind");
+            Console.WriteLine("I am a Shopaholic person");
+            Console.WriteLine("Whenever I am free I go for Shopping");
+            Console.WriteLine("Otherwise I feel bore at home");
+            Console.WriteLine("I am an International student");
+            Console.WriteLine("Some of my friends are in Canada");
+            Console.WriteLine("I live in Lemoore");
+            Console.WriteLine("I am a punctual and studious student");
+            Console.WriteLine("I believe that education is the key to success");
+            Console.WriteLine("I am majoring in Liberal Arts of Math and Science");
+            Console.WriteLine("I joined West Hills College in 2022");
+            Console.WriteLine("This is my Third year at West Hills college");
+            Console.WriteLine("I plan to transfer to a 4-year university for Computer Science");
+            Console.WriteLine("I am a good student");
+            Console.WriteLine("I love to play sport");
+            Console.WriteLine("My favorite subjects are Mathematics and Science");
+            Console.WriteLine("I am working in a medical transportation part time job");
+            Console.WriteLine("I learned various skills from my work");
+            Console.WriteLine("It has taught me manycrucial qualities needed to grow into a better person");
+            Console.WriteLine("I learned how to balance my life");
+            Console.WriteLine("I mantained my college, work and family life");
+            Console.WriteLine("I have matured in the sense of becomingwise enough to make my own decisions about myself");
+            Console.WriteLine("My parents are the biggest supporter who always motivates me");
+            Console.WriteLine("I have many goals and dreams in life");
+            Console.WriteLine("My goal is to become an software engineer");
+            Console.WriteLine("I want to give a lavish lifestyle to my parents");
+            Console.WriteLine("This is reason why I always study my lessons");
+            Console.WriteLine("I want to become an Independent women");
+            Console.WriteLine("I believe studying hard is the ladder in the attainments of my dreams");
+            Console.WriteLine("The dreams of giving my family a better life");
+            Console.WriteLine("Whenever I work I give my 100percent");
+            Console.WriteLine("No I am not selfish,but in life I do what is best for me");
+            Console.WriteLine("I am very happy to be here and learning new things everyday");
+            Console.WriteLine("I think of life as the ink in a pen");
+            Console.WriteLine("A pen that neverleaves a paper with a limited amount of ink");
+            Console.WriteLine("I think life is too short we should enjoy it how much god had given to us");
+
+        }
+    }
+}
